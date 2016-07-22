298fd123ca948fbba6f9e7a2693c8fe29c7ee611
kodamaa2 <andreas.stiller1@googlemail.com>
2016-07-23 00:03:14 +0200

    sry

--- app/src/main/java/com/stiller/andreas/testapp/MainActivity.java
+++ app/src/main/java/com/stiller/andreas/testapp/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends ActionBarActivity {
         // Julius ist ein grosser Pisser
         // Julius stinkt auch nach scheisse
         // Wenn der Andi noch einmal mich beleidigt steige ich aus
-        // ok tut mir leid julius
+        // ok tut mir leid Julius
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
