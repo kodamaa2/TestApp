af7b02c393199fc5548da508649a40ac89ba308b
kodamaa2 <andreas.stiller1@googlemail.com>
2016-07-22 23:41:44 +0200

    julius stinkt nach scheisse

--- app/src/main/java/com/stiller/andreas/testapp/MainActivity.java
+++ app/src/main/java/com/stiller/andreas/testapp/MainActivity.java
@@ -17,8 +17,8 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        // Julius ist ein groﬂer Pisser
-
+        // Julius ist ein grosser Pisser
+        // Julius stinkt auch nach scheisse
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
