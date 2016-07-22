49905aeb9bf0f3cab2b5af1dd365c13f23fdcf30
kodamaa2 <andreas.stiller1@googlemail.com>
2016-07-22 23:34:42 +0200

    Julius ist ein großer Pisser

--- app/src/main/java/com/stiller/andreas/testapp/MainActivity.java
+++ app/src/main/java/com/stiller/andreas/testapp/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        // Julius ist ein Pisser
+        // Julius ist ein groﬂer Pisser
 
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
